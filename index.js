46909f1ed0ef0db73639160df978442a82ead711
Nikolay Georgiev <nikolay@georgiev.io>
2014-09-30 06:39:07 +0200

    0.0.1 - Added german quick test data and few tests.

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var fs = require('fs');
+
+/* the object to be exported */
+var result = {
+  langs: []
+};
+
+var dataDir = "data/";
+fs.readdir(dataDir, function(err, fileNames) {
+  if (err) throw err;
+
+  fileNames.forEach(function(fileName) {
+
+    fs.readFile(dataDir + fileName, function(err, data) {
+      if (err) throw err;
+
+      var lang = fileName.slice(-7, -5);
+      var json = JSON.parse(data);
+
+      result.langs.push(lang);
+      result[lang] = json;
+    });
+  });
+
+});
+
+module.exports = result;
---
+++ test/tests.spec.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var expect = require('chai').expect;
+var data = require('../index');
+var fs = require('fs');
+
+describe('Data Files', function() {
+
+  it('should have correct file names', function() {
+
+    var dataDir = "data/";
+    fs.readdir(dataDir, function(err, fileNames) {
+      if (err) throw err;
+      for (var i = 0; i < fileNames.length; i++) {
+        var fileName = fileNames[i];
+        expect(fileName.slice(-5)).to.equal('.json');
+      }
+    });
+
+  });
+});
+
+describe('Result Object', function() {
+
+  it('should contain language data', function() {
+    expect(data).to.exist;
+    expect(data.de).to.exist;
+    expect(data.en).to.exist;
+  });
+
+  it('should have translated names of the quick test', function() {
+    expect(data.de.name).to.equal('Schnelltest Gemeinwohl-Ökonomie');
+    expect(data.en.name).to.equal('ECG Quick Test');
+  });
+
+  it('should contain 27 questions', function() {
+    expect(data.de.test.questions.length).to.equal(27);
+    expect(data.en.test.questions.length).to.equal(27);
+  });
+
+});
