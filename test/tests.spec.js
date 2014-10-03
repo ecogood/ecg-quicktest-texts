85acebb9a11521bf8f663beee24b2a2ed1dcf795
Nikolay Georgiev <nikolay@georgiev.cc>
2014-10-03 15:54:39 +0200

    0.0.4 - added module tests and grunt

--- test/tests.spec.js
+++ test/tests.spec.js
@@ -18,19 +18,93 @@ describe('Data Files', function() {
     });
 
   });
+
 });
 
-describe('Result Object', function() {
+describe('Module', function() {
 
-  it('should contain language data', function() {
+  var langs = [];
+
+  beforeEach(function() {
+    Object.keys(data).forEach(function(dataKey) {
+      if (dataKey !== 'langs') {
+        var lang = data[dataKey];
+        langs.push(lang);
+      }
+    });
+  });
+
+  it('should contain data', function() {
     expect(data).to.exist;
+  });
+
+  it('should contain German texts', function() {
     expect(data.de).to.exist;
-    expect(data.en).to.exist;
+  });
+
+  it('should have matrix version 4.1', function() {
+    langs.forEach(function(lang){
+      expect(lang.matrixVersion).to.equal('4.1');
+    });
+  });
+
+  it('should have correct structure.participants.type', function() {
+    langs.forEach(function(lang){
+      var participants = lang.structure.participants;
+      participants.forEach(function(participant){
+        expect(['company', 'oneperson']).to.include(participant.type);
+      });
+    });
+  });
+
+  it('should have correct structure.individualAnswer.evaluationValues.value', function() {
+    langs.forEach(function(lang){
+      var evaluationValues = lang.structure.individualAnswer.evaluationValues;
+      for (var i = 0; i < evaluationValues.length; i++) {
+        var evaluationValue = evaluationValues[i];
+        expect(evaluationValue.value).to.equal(i);
+      }
+    });
+  });
+
+  it('should have correct structure.result.feedback.minValue/maxValue', function() {
+    langs.forEach(function(lang){
+      var feedbacks = lang.structure.result.feedback;
+      for (var i = 0; i < feedbacks.length; i++) {
+        var feedback = feedbacks[i];
+        if (i === 0) {
+          expect(feedback.minValue).to.equal(0);
+          expect(feedback.maxValue).to.equal(32);
+        } else if (i === 1) {
+          expect(feedback.minValue).to.equal(33);
+          expect(feedback.maxValue).to.equal(62);
+        } else if (i === 2) {
+          expect(feedback.minValue).to.equal(63);
+          expect(feedback.maxValue).to.equal(94);
+        } else if (i === 3) {
+          expect(feedback.minValue).to.equal(95);
+          expect(feedback.maxValue).to.equal(126);
+        }
+      }
+    });
+  });
+
+  it('should have correct test.questions.stakeholders/ecgValue', function() {
+    langs.forEach(function(lang){
+      var questions = lang.test.questions;
+      for (var i = 0; i < questions.length; i++) {
+        var question = questions[i];
+        expect(['A', 'B', 'C', 'D', 'E']).to.include(question.stakeholders);
+        expect(['1', '2', '3', '4', '5']).to.include(question.ecgValue);
+      }
+    });
   });
 
   it('should contain 27 questions', function() {
-    expect(data.de.test.questions.length).to.equal(27);
-    expect(data.en.test.questions.length).to.equal(27);
+    langs.forEach(function(lang){
+      var questions = lang.test.questions;
+      expect(questions.length).to.equal(27);
+    });
   });
 
 });
